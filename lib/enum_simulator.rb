115dc79e7d261e944b1ffd2e8967f4e3c5a87379
Nick <nick@frontofficemedia.com>
2011-12-29 13:31:09 -0500

    Fixed a "method not found" error

--- lib/enum_simulator.rb
+++ lib/enum_simulator.rb
@@ -7,7 +7,7 @@ module EnumSimulator
   module ClassMethods
     def enum(attr, values)
       # pushes 'nil' into the values array if the column definition allows null values
-      values << nil if self.columns_hash[attr.to_s].null
+      values << nil if self.columns_hash[attr.to_s].respond_to? 'null' and self.columns_hash[attr.to_s].null
       @enumerated_attributes ||= {}
       @enumerated_attributes[attr] = values
       validates_inclusion_of attr, :in => values
